78021ff1f1141339744a865c3d7fefb8335b6f83
robbie <robbieveivers@outlook.com>
2024-03-16 02:01:46 +0000

    Step 1.3 Python File

---
+++ 3 - Introduction to Kubernetes/Challenges/1.3/mycontainerapp.py
@@ -0,0 +1,14 @@
+# Import the necessary modules
+from flask import Flask
+
+# Define the Flask app
+app = Flask(__name__)
+
+# Define the view
+@app.route('/')
+def home():
+    return "Welcome to Robbies app."
+
+# Run the app on port 80, with debug
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
